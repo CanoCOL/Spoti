34f2364a7010a3c467c99c514e5472649d161f13
407 <407@academicos.cesde.edu.co>
2022-09-29 14:10:40 -0500

    consumiendo una Api

---
+++ src/helpers/constantesGET.js
@@ -0,0 +1,7 @@
+const TOKEN = "Bearer BQA58T4h13jr9aWyooPcRv-PdmEfBiMpiosG7aFD_FVDRoqfRG31Cd9joBvEZwe9_x0DCAmjp1OwfjGdLS2vbyKzI1biuF4m8HC2XrprVZPsWpqSPyeWSLi_zBSTguBB1If2oV1yrIB18k9-9UkIzDdFKwVA1uLl8Z_cABcpGAdFuWWXKPAV319vfSzwIQc8NPk"
+
+export const PETICION = {
+    method:"GET",
+    headers:{Authorization:TOKEN}
+
+}
